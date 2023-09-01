f0d05564f81d14c9d4383d5c8d5f642339f8604f
Mandar Junnarkar <mandarjunnarkar222@gmail.com>
2023-09-01 09:37:00 +0000

    1.1

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -11,8 +11,8 @@ function ExpenseItem(props) {
         <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">₹{props.amount}</div>
-        <div className="expense-item_delete" onClick={()=>props.deleteExpenseItem(props.id)}>Delete</div>
         </div>
+        <div className="expense-item_delete" onClick={()=>props.deleteExpenseItem(props.id)}>Delete</div>
       </Card>
     </li>
   );
